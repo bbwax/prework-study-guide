865ba7d5f789fc81c0564a28b6746c765afe271f
bwax <braxton.waxdeck@gmail.com>
2022-09-06 17:35:14 -0700

    fix accidental nested directories

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,21 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 function listTopics() {
-    for (var x = 0; x < topics.length; x++) {
+  for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
-   }
+  }
 }
 function selectTopic() {
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+  if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+  var shapes = ["triangle", "square", "pentagon", "circle"];
+  console.log(shapes[0]); 
 }
-var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]); 
